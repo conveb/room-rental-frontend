669fd622aa1793bd450052fa8b4df5d1a45d76b3
edvin <edvinjohnson333@gmail.com>
2025-12-19 14:20:55 +0530

    testing9

--- src/hooks/auth/useSignup.js
+++ src/hooks/auth/useSignup.js
@@ -11,14 +11,11 @@ export const useSignup = () => {
   const [error, setError] = useState("");
 
   // STEP 1 — SEND OTP
-  const sendOtp = async (email) => {
+  const sendOtp = async ({ email, purpose }) => {
     setError("");
     try {
       setLoading(true);
-      const res = await sendOtpAPI({
-        email,
-        purpose: "ONBOARDING",
-      });
+      const res = await sendOtpAPI({ email, purpose });
       console.log(res.data.message);
       return res;
     } catch (err) {
@@ -29,24 +26,21 @@ export const useSignup = () => {
     }
   };
 
-const verifyOtp = async (payload) => {
-  setError("");
-  try {
-    setLoading(true);
-    const res = await verifyOtpAPI(payload);
-
-    console.log("OTP VERIFIED:", res.data);
-
-    return res;
-  } catch (err) {
-    setError("Invalid OTP");
-    console.error(err?.response?.data || err);
-    throw err;
-  } finally {
-    setLoading(false);
-  }
-};
-
+  const verifyOtp = async (payload) => {
+    setError("");
+    try {
+      setLoading(true);
+      const res = await verifyOtpAPI(payload);
+      console.log("OTP VERIFIED:", res.data);
+      return res;
+    } catch (err) {
+      setError("Invalid OTP");
+      console.error(err?.response?.data || err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
 
   // STEP 3 — REGISTER USER
   const register = async (payload) => {
@@ -54,15 +48,13 @@ const verifyOtp = async (payload) => {
     try {
       setLoading(true);
       const res = await signupAPI(payload);
-      console.log(res.data)
+      console.log(res.data);
       return res;
     } catch (err) {
       const data = err?.response?.data;
-
       let message = "Registration failed";
       if (Array.isArray(data?.email)) message = data.email[0];
       else if (data?.detail) message = data.detail;
-
 
       setError(message);
       throw err;
@@ -72,11 +64,15 @@ const verifyOtp = async (payload) => {
   };
 
   // RESEND OTP
-  const resendOtp = async (email) => {
-    return await resendOtpAPI({
-      email,
-      purpose: "ONBOARDING",
-    });
+  const resendOtp = async ({ email, purpose }) => {
+    try {
+      const res = await resendOtpAPI({ email, purpose });
+      console.log(res.data.message);
+      return res;
+    } catch (err) {
+      setError(err?.response?.data?.detail || "Failed to resend OTP");
+      throw err;
+    }
   };
 
   return {
